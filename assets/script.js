cce857cdedc31b2396f628ccfd852f57de59a91a
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-02-08 20:44:21 -0800

    testing javascript

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,21 @@
+var weatherData = null;
+
+getWeatherData();
+
+console.log(weatherData);
+
+async function getWeatherData() {
+    var lat = '47.6062'
+    var lon = '122.3321'
+
+    console.log('taco')
+
+    const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=32bd8e74f5dedfd3739fe74114309756');
+
+    weatherData = await response.json();
+
+    console.log(weatherData); 
 
 
+
+}
